f5737de1825494095b9b793889b69fc0115bd24a
Devkant Swargiary <40569250+Devkant21@users.noreply.github.com>
2021-10-19 23:31:02 +0530

    Add files via upload

--- ticket.py
+++ ticket.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///jobs.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db= SQLAlchemy(app)
 
+class JOBS(db.Model):
+    SLNo = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(200), nullable=False)
+    Description = db.Column(db.String(500), nullable=False)
+    Date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+
+    def __repr__(self) -> str:
+        return f" {self.SLNo} - {self.Title} " 
+
+         
 @app.route("/")
 def hello_world():
+    jobs = JOBS(Title="First Jobs", Description="Invest in SM")
+    db.session.add(jobs)
+    db.session.commit()
     return render_template('index.html')
 
 @app.route("/products")
